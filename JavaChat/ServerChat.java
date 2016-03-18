1d980494ba3d103ca3f249a22e0d6868fd4d7755
moonlight83340 <moonlight83340@users.noreply.github.com>
2016-03-18 12:16:06 +0100

    Create ServerChat.java

---
+++ JavaChat/ServerChat.java
@@ -0,0 +1,35 @@
+package reseau_tp_1;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerChat {
+	private int port;
+
+	
+	public ServerChat(int port) throws IOException{
+		this.port = port;
+		run();
+	}
+	
+	private void run() throws IOException{
+		Socket client;
+		ServerSocket server = new ServerSocket(this.port);
+		for(;;){
+			client = server.accept();
+			ThreadServer thread = new ThreadServer(client);
+			thread.start();
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			ServerChat server = new ServerChat(5015);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
