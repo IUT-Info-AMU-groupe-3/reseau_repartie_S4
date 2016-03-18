3642eccdf49b63db96d804c310bdbd767ea7e667
moonlight83340 <moonlight83340@users.noreply.github.com>
2016-03-18 09:01:16 +0100

    Create server_tcp.java

---
+++ TP/SERVEUR/server_tcp.java
@@ -0,0 +1,44 @@
+package reseau_tp_1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.GregorianCalendar;
+
+public class ServerTcp {
+	private int port;
+	
+	public ServerTcp(int port) throws IOException{
+		this.port = port;
+		run();
+	}
+	
+	private void run() throws IOException{
+		Socket client;
+		BufferedWriter out;
+		BufferedReader in;
+		ServerSocket server = new ServerSocket(this.port);
+		for(;;){
+			client = server.accept();
+			out = new BufferedWriter(new OutputStreamWriter(
+					client.getOutputStream()));
+			String date = new GregorianCalendar().getTime().toString();
+			out.write(date);
+			out.newLine();
+			out.flush();
+			client.close();
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			ServerTcp tcp_server = new ServerTcp(5014);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
