4bd19cf561e43573b3f3674c92c7445bb7217a6b
moonlight83340 <moonlight83340@users.noreply.github.com>
2016-03-25 12:13:24 +0100

    Update ClientChatMulticast.java

--- TP/CLIENT/ClientChatMulticast.java
+++ TP/CLIENT/ClientChatMulticast.java
@@ -7,17 +7,21 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.swing.JOptionPane;
+
 public class ClientChatMulticast {
 	final public static int PORT = 2000;
-	final public static String HOST = "238.0.0.23";
+	final public static String HOST = "238.0.0.25";
 	
 	public static void main(String[] args) throws IOException {
 
         final FenetreChat fc = new FenetreChat("La folie du chat");
-        
+        String pseudo = JOptionPane.showInputDialog("Pseudo", "Poireau Volant") + " : ";
+        Scanner sc = new Scanner(System.in);
         final MulticastSocket socket = new MulticastSocket(PORT);
         socket.joinGroup(InetAddress.getByName(HOST));
         
@@ -25,7 +29,7 @@ public class ClientChatMulticast {
             @Override
             public void keyPressed(KeyEvent e) {
                 if(e.getKeyCode() == KeyEvent.VK_ENTER){
-                    byte[] msg = fc.getMessage().getBytes();
+                    byte[] msg = (pseudo + fc.getMessage()).getBytes();
                     try {
                         DatagramPacket packet = new DatagramPacket(
                                 msg,
